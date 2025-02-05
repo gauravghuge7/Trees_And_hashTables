c6b53b45832e2712a85f66197649749f161f6116
gaurav ashok ghuge <guduughuge7@gmail.com>
2025-02-05 12:32:26 +0530

    update the diameter of tree

---
+++ BinaryTrees/TreeNode/TreeNode.java
@@ -0,0 +1,11 @@
+package BinaryTrees.TreeNode;
+
+public class TreeNode {
+
+    public TreeNode left;
+    public TreeNode right;
+    int val;
+    public TreeNode(int val) {
+        this.val = val;
+    }
+}
